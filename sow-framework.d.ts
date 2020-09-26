4db410b9f7a03721be5f90b83032565fb15d5acd
Rajib Chowdhury <mysuccessstairs@hotmail.com>
2020-09-26 21:50:30 +0600

    add typecast v0.0.1

--- sow-framework.d.ts
+++ sow-framework.d.ts
@@ -9,47 +9,47 @@
 import { OpenNewWindowConfig } from './sow-core';
 import HandsonTable from 'handsontable';
 declare interface IAssembler {
-	Class<T>( ...args: T[] ): T;
-	Class<T>( arg1: T ): T;
-	Class<T, S>( arg1: T, arg2: S ): T & S;
-	Class<T, S, Z>( arg1: T, arg2: S, arg3: S ): T & S & Z;
-	aggregate<T>( ...args: ( () => T )[] ): T;
-	aggregate<T, S>( arg1: () => T, arg2: () => S ): T & S;
-	aggregate<T, S, Z>( arg1: () => T, arg2: () => S, arg3: () => Z ): T & S & Z;
-	extend<T, S>( dest: T | ( () => T ), source: S | ( () => S ) ): T & S;
-	Closure( ...args: ( () => void )[] ): IAssembler;
+	Class<T>(...args: T[]): T;
+	Class<T>(arg1: T): T;
+	Class<T, S>(arg1: T, arg2: S): T & S;
+	Class<T, S, Z>(arg1: T, arg2: S, arg3: S): T & S & Z;
+	aggregate<T>(...args: (() => T)[]): T;
+	aggregate<T, S>(arg1: () => T, arg2: () => S): T & S;
+	aggregate<T, S, Z>(arg1: () => T, arg2: () => S, arg3: () => Z): T & S & Z;
+	extend<T, S>(dest: T | (() => T), source: S | (() => S)): T & S;
+	Closure(...args: (() => void)[]): IAssembler;
 }
 declare class Assembler {
 	public readonly Create: IAssembler;
 }
 export declare interface AssemblerConstructor {
-    new(  ): Assembler;
-    readonly prototype: Assembler;
+	new(): Assembler;
+	readonly prototype: Assembler;
 }
 declare interface IHook {
-	hook( schema: "__web__page" ): IHook;
-	hook( schema?: string ): IHook;
-	add( name: "__open__new", fn: ( cfg: OpenNewWindowConfig ) => void ): IHook;
-	add( name: "on_route_change", fn: ( a: string, $b: JQuery<HTMLElement>, script?: string, c?: ( status: string ) => void, isdialog?: boolean, $ui?: JQueryUI.Dialog ) => void ): IHook;
-	add( name: string, fn: ( ...args: any[] ) => void ): IHook;
-	fire( evt: string, args: any[] ): IHook;
-	fire( evt: "__open__new", args: OpenNewWindowConfig[] ): IHook;
-	firea( evt: "on_route_change", a: string, $b: JQuery<HTMLElement>, script?: string, c?: ( status: string ) => void, isdialog?: boolean, $ui?: JQueryUI.Dialog );
-	firea( ...args: any[] ): IHook;
+	hook(schema: "__web__page"): IHook;
+	hook(schema?: string): IHook;
+	add(name: "__open__new", fn: (cfg: OpenNewWindowConfig) => void): IHook;
+	add(name: "on_route_change", fn: (a: string, $b: JQuery<HTMLElement>, script?: string, c?: (status: string) => void, isdialog?: boolean, $ui?: JQueryUI.Dialog) => void): IHook;
+	add(name: string, fn: (...args: any[]) => void): IHook;
+	fire(evt: string, args: any[]): IHook;
+	fire(evt: "__open__new", args: OpenNewWindowConfig[]): IHook;
+	firea(evt: "on_route_change", a: string, $b: JQuery<HTMLElement>, script?: string, c?: (status: string) => void, isdialog?: boolean, $ui?: JQueryUI.Dialog);
+	firea(...args: any[]): IHook;
 }
 declare interface IHookFunc {
-	( name: string ): IHook;
-	remove( evt: string ): IHook;
+	(name: string): IHook;
+	remove(evt: string): IHook;
 }
 declare interface Ihub {
 	getServerProxy(): string[];
 	socket(): any;
-	init( needMsgInit?: boolean ): void;
+	init(needMsgInit?: boolean): void;
 }
 declare interface IDataMap {
-	set<T>( mom: T, child?: any, value?: any ): IDataMap;
-	get( mom?: string, child?: any, deep?: boolean, deeper?: boolean ): any;
-	push( mom: string, child: any, value?: any ): IDataMap;
+	set<T>(mom: T, child?: any, value?: any): IDataMap;
+	get(mom?: string, child?: any, deep?: boolean, deeper?: boolean): any;
+	push(mom: string, child: any, value?: any): IDataMap;
 	clean(): IDataMap;
 	clear(): IDataMap;
 }
@@ -60,23 +60,23 @@ declare interface IShow {
 	 * @param {any} msg
 	 * @returns {IShow}
 	 */
-	i( msg?: any ): this;
+	i(msg?: any): this;
 	/** Show success notification
 	 * @param {any} msg
 	 * @returns {IShow}
 	 */
-	s( msg?: any ): this;
+	s(msg?: any): this;
 	/** Show error notification
 	 * @param {any} msg
 	 * @returns {IShow}
 	 */
-	e( msg?: any ): this;
+	e(msg?: any): this;
 }
-declare type ModuleRequire = ( name: string ) => any;
-declare type IModules = { [id: string]: [( require: ModuleRequire, module: IAssembler, exports: Dct<any> ) => Dct<any>, Dct<any>] };
+declare type ModuleRequire = (name: string) => any;
+declare type IModules = { [id: string]: [(require: ModuleRequire, module: IAssembler, exports: Dct<any>) => Dct<any>, Dct<any>] };
 declare interface IDate {
 	get(): string;
-	getLocal( offset: string, now?: Date ): Date;
+	getLocal(offset: string, now?: Date): Date;
 	getMap(): {
 		month: { [id: number]: string };
 		day: { [id: number]: string };
@@ -103,34 +103,36 @@ interface IBrowser {
 	name: string;
 	type: string;
 	version: number;
-	support( cfg: Dct<any> ): void;
+	support(cfg: Dct<any>): void;
 }
 declare interface IBrowserWindow {
 	/** Reg/Remove browser window resize event */
-	onResize( type: string | ( () => void ) ): string | void;
+	onResize(type: string | (() => void)): string | void;
 }
 declare interface IHotTable {
-	createColumnWidth( hotSettings:HandsonTable.GridSettings, containerWidth:number): number[];
+	createColumnWidth(hotSettings: HandsonTable.GridSettings, containerWidth: number): number[];
 	/** Resize Handsontable after 300ms */
-	resize( hot: HandsonTable, orgWidths: number[], $elm: JQuery<HTMLElement> ): void;
+	resize(hot: HandsonTable, orgWidths: number[], $elm: JQuery<HTMLElement>): void;
 }
 export declare interface ISow {
 	OS: 'Windows' | 'Mobile' | 'Linux';
 	hub: Ihub;
 	Window: IBrowserWindow;
-	async( callback: ( ...args: any[] ) => void, ms: number | void ): void;
-	registerNamespace( name: string, callback: () => [IModules, Dct<any>, string[]] ): ISow;
-	mapPageNamespace( ...args: any[] ): ISow;
-	exportModule( name: string ): any;
-	reRegisterNamespace( name: string ): ISow;
-	exportNamespace( name: string ): any;
-	mapNamespace( parent: string, child: string ): ISow;
-	namespaceExists( name: string ): boolean;
-	requirePublicModule( namespaceName: string, moduleName?: string ): any;
-	define<T>( name: string, fun: ( () => T ) | Dct<T> ): ISow;
-	Web: import( './sow-core' ).IWeb;
-	usingNamespace( name: string ): ISow;
-	unloadNamespace( name: string ): ISow;
+	async(callback: (...args: any[]) => void, ms: number | void): void;
+	registerNamespace(name: string, callback: () => [IModules, Dct<any>, string[]]): ISow;
+	mapPageNamespace(...args: any[]): ISow;
+	exportModule(name: string): any;
+	reRegisterNamespace(name: string): ISow;
+	exportNamespace(name: string): any;
+	mapNamespace(parent: string, child: string): ISow;
+	namespaceExists(name: string): boolean;
+	requirePublicModule(namespaceName: string, moduleName?: string): any;
+	/** Type cast from `T` to `any` */
+	toAny<T>(obj: T): any;
+	define<T>(name: string, fun: (() => T) | Dct<T>): ISow;
+	Web: import('./sow-core').IWeb;
+	usingNamespace(name: string): ISow;
+	unloadNamespace(name: string): ISow;
 	Assembler: AssemblerConstructor;
 	hook: IHookFunc;
 	App: {
@@ -148,11 +150,11 @@ export declare interface ISow {
 		hide(): void;
 	};
 	checkPrivacy(): string;
-	onXHRError( jqXHR: JQueryXHR, statusCode: number, textStatus?: string ): {
+	onXHRError(jqXHR: JQueryXHR, statusCode: number, textStatus?: string): {
 		fw: boolean; emsg?: string; status_code: number; status_description?: string;
 	};
-	JSON( obj: any ): any[];
-	parseParam( obj: any ): Dct<any>;
+	JSON(obj: any): any[];
+	parseParam(obj: any): Dct<any>;
 	date: IDate;
 	currentPage(): string;
 	sound: {
@@ -165,9 +167,9 @@ export declare interface ISow {
 		readonly prototype: IData;
 	};
 	Static: {
-		all( obj: Dct<any> ): any;
-		define( obj: Dct<any>, poperty: string, value: any ): any;
-		change( obj: Dct<any>, poperty: string, value: any ): any;
+		all(obj: Dct<any>): any;
+		define(obj: Dct<any>, poperty: string, value: any): any;
+		change(obj: Dct<any>, poperty: string, value: any): any;
 	};
 	browser: IBrowser;
 	dom: {
@@ -177,17 +179,17 @@ export declare interface ISow {
 		isSmartPhone: boolean;
 		isTablet: boolean;
 	};
-	export( nam: string ): any;
-	isArrayLike<T>( obj?: any ): obj is T[];
-	isDate( val: any ): val is Date;
-	isError( obj ): obj is Error;
-	isPlainObject<T>( obj?: any ): obj is Dct<T>;
+	export(nam: string): any;
+	isArrayLike<T>(obj?: any): obj is T[];
+	isDate(val: any): val is Date;
+	isError(obj): obj is Error;
+	isPlainObject<T>(obj?: any): obj is Dct<T>;
 	multi: {
-		inherit( ...args: any[] ): any;
+		inherit(...args: any[]): any;
 	};
-	onRouterChange( event: Dct<any> ): void;
-	remove( name: string ): ISow;
-	require( name: string ): any;
+	onRouterChange(event: Dct<any>): void;
+	remove(name: string): ISow;
+	require(name: string): any;
 	hot: IHotTable;
-	widgetTemplate():string;
+	widgetTemplate(): string;
 }
