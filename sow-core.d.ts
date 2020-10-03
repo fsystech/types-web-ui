d3d170ef3722461fad193a41b77b9bcab8fdaaf2
Rajib Chowdhury <mysuccessstairs@hotmail.com>
2020-10-03 14:14:59 +0600

    add ext param in DropDef v0.0.1

--- sow-core.d.ts
+++ sow-core.d.ts
@@ -229,6 +229,7 @@ export declare interface ISelectizeConfig {
 }
 export declare type DropDef = {
     url?: string; sp: ((pageCtx: IPageContext) => void) | string;
+    param?: (pageCtx: IPageContext, obj: { query: string, vtype: string }) => Dct<any>;
     selectize_config?: ISelectizeConfig;
 };
 declare type SourceType = {
