7c89e6c297650977d9a3a4b54cdc36c10875573b
Rajib Chowdhury <mysuccessstairs@hotmail.com>
2020-11-03 22:10:46 +0600

    Update jQDataTable->addDefaultButtons

--- sow-core.d.ts
+++ sow-core.d.ts
@@ -18,6 +18,7 @@ declare type SearchDetail = {
     poperty?: any[];
     tablesorter?: boolean;
     jQDataTable?: {
+        addDefaultButtons?: boolean;
         settings?: DataTables.Settings;
         table?: DataTables.Api;
     };
